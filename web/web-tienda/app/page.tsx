b4fce1cd5afa974cdba51e11743f277d54de9329
samuelvergaramartin <accesopuerta0@gmail.com>
2026-09-09 19:02:03 +0200

    feat: Añadida la navbar  a la página principal

--- web/web-tienda/app/page.tsx
+++ web/web-tienda/app/page.tsx
@@ -1,8 +1,10 @@
 import { env } from 'node:process';
 import PayPalPage from './components/PayPalPage';
+import NavBar from './components/NavBar';
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br from-gray-800 via-gray-700 to-gray-800 text-white">
+      <NavBar />
       <PayPalPage clientId={env["PAYPAL_CLIENT_ID"]!}/>
     </div>
   );
