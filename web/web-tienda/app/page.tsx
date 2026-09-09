86917f180813d40ac119ca179d7e588f77b66ece
samuelvergaramartin <accesopuerta0@gmail.com>
2026-09-09 20:04:50 +0200

    feat: Implementado PaypalButtons en la página principal

--- web/web-tienda/app/page.tsx
+++ web/web-tienda/app/page.tsx
@@ -1,11 +1,11 @@
 import { env } from 'node:process';
-import PayPalPage from '@/app/components/PayPalPage';
+import PaypalButtons from '@/app/components/PaypalButtons';
 import NavBar from '@/app/components/NavBar';
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-800 via-gray-700 to-gray-800 text-white">
       <NavBar />
-      <PayPalPage clientId={env["PAYPAL_CLIENT_ID"]!}/>
+      <PaypalButtons clientId={env["PAYPAL_CLIENT_ID"]!}/>
     </div>
   );
 }
